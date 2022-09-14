9e1e5961407ceb62ae3da0239ba27d7a3316bab8
Francielle Rodrigues <francieller20@gmail.com>
2022-09-13 23:33:11 -0300

    lista7 atualizada

--- src/app/pages/contato/contato.component.ts
+++ src/app/pages/contato/contato.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contato',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContatoComponent implements OnInit {
 
-  constructor() { }
+  formulario: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formulario = this.formBuilder.group({
+      nome: ['', [Validators.required, Validators.pattern("^[A-Z ]+$")]],
+      telefone: [''],
+      email: ['', [Validators.required, Validators.email]],
+      cep: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(8), Validators.pattern("^[0-9]*$")]],
+      assunto: ['', [Validators.required, Validators.maxLength(50)]],
+      mensagem: ['', [Validators.required, Validators.maxLength(500)]]
+    }); 
+   }
 
   ngOnInit(): void {
+  }
+
+  public enviar(){
+    console.log(this.formulario.get('nome')?.value);
+    console.log(this.formulario.get('telefone')?.value);
+    console.log(this.formulario.get('email')?.value);
+    console.log(this.formulario.get('cep')?.value);
+    console.log(this.formulario.get('assunto')?.value);
+    console.log(this.formulario.get('mensagem')?.value);
   }
 
 }
